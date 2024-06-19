f361a86078160722d2d29607b73e01e5d97bc3d2
daichitakahashi <t.daichi.snd@gmail.com>
2024-06-19 13:50:58 +0900

    feat: generate test results

--- test-ci.sh
+++ test-ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-go test ./... -coverprofile=coverage.out -coverpkg=./... || exit 1;
+gotestsum --format=testname --junitfile=test-results.xml -- ./... -coverprofile=coverage.out -coverpkg=./... || exit 1;
 go tool cover -html=coverage.out -o=coverage.html
 echo ""
 go tool cover -func=coverage.out
